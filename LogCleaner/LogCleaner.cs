ad5ac1e96b088bf3fccba2acb1b5c7c902d53e07
art0007i <art0007i@gmail.com>
2024-04-28 18:22:06 +0200

    remove a bunch of spams related to contact status

--- LogCleaner/LogCleaner.cs
+++ LogCleaner/LogCleaner.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using FrooxEngine;
 using System.Reflection.Emit;
 using Elements.Core;
+using SkyFrost.Base;
+using System.Runtime.CompilerServices;
 
 namespace LogCleaner
 {
@@ -15,7 +17,7 @@ namespace LogCleaner
     {
         public override string Name => "LogCleaner";
         public override string Author => "art0007i";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/art0007i/LogCleaner/";
 
         public static HashSet<MethodInfo> logMethods = new HashSet<MethodInfo>
@@ -45,37 +47,70 @@ namespace LogCleaner
             typeof(ShadowCastMode)
         };
 
-        public static HashSet<MethodInfo> toPatch = new HashSet<MethodInfo>
+        // functions in here will still print messages, but not stack traces
+        public static HashSet<MethodInfo> removeStackTrace = new HashSet<MethodInfo>
         {
             // This happens whenever you change name or description of a world (often triggered without interaction)
             AccessTools.Method(typeof(WorldConfiguration), "FieldChanged"),
-
-            // if you want more stack traces to be stopped just add them here!
         };
+
+        // functions in here will be completely silenced
+        public static HashSet<MethodInfo> removeLog = new HashSet<MethodInfo>
+        {
+            // Very spammy and doesn't seem like it has any useful info
+            AccessTools.Method(typeof(ContactData), "UpdateStatus"),
+            AccessTools.Method(typeof(ContactData), "ClearExpired"),
+            AccessTools.Method(typeof(UserStatusManager), "SendStatusToUser"),
+            FindAsyncBody(AccessTools.Method(typeof(AppHub), "BroadcastStatus")),
+        };
+
+        public static MethodInfo FindAsyncBody (MethodInfo mi)
+        {
+            AsyncStateMachineAttribute asyncAttribute = (AsyncStateMachineAttribute)mi.GetCustomAttribute(typeof(AsyncStateMachineAttribute));
+            Type asyncStateMachineType = asyncAttribute.StateMachineType;
+            return AccessTools.Method(asyncStateMachineType, nameof(IAsyncStateMachine.MoveNext));
+        }
 
         public override void OnEngineInit()
         {
-
-
             Harmony harmony = new Harmony("me.art0007i.LogCleaner");
-            //harmony.PatchAll();
+
+            Debug("Patching ctx menu funcs");
+            // Patch context menu stuffs
             var GenericToPatch = typeof(RadialMenuItemExtensions).GetMethod(nameof(RadialMenuItemExtensions.AttachOptionDescriptionDriver));
             var patcherMethod = new HarmonyMethod(AccessTools.Method(typeof(ContextMenuOpenPatch), nameof(ContextMenuOpenPatch.Transpiler)));
             foreach (var item in ctxMenuTypes)
             {
                 var toPatch = GenericToPatch.MakeGenericMethod(item);
                 harmony.Patch(toPatch, transpiler: patcherMethod);
-
             }
 
+            Debug("Patching stack trace funcs");
+            // Remove stack traces
             MethodInfo transpiler = typeof(StackTraceFixerPatch).GetMethod(nameof(StackTraceFixerPatch.Transpiler));
-
-            foreach (MethodInfo method in toPatch)
+            foreach (MethodInfo method in removeStackTrace)
             {
                 Debug("Patching method " + method);
                 try
                 {
                     harmony.Patch(method, transpiler: new HarmonyMethod(transpiler));
+                }
+                catch (Exception e)
+                {
+                    Debug("  error in patch for " + method);
+                    Debug(e);
+                }
+            }
+
+            Debug("Patching remove log funcs");
+            // Remove log methods
+            MethodInfo transpiler2 = typeof(RemoveLogMethodPatch).GetMethod(nameof(RemoveLogMethodPatch.Transpiler));
+            foreach (MethodInfo method in removeLog)
+            {
+                Debug("Patching method " + method);
+                try
+                {
+                    harmony.Patch(method, transpiler: new HarmonyMethod(transpiler2));
                 }
                 catch (Exception e)
                 {
@@ -123,7 +158,27 @@ namespace LogCleaner
                 {
                     if(code.operand is MethodInfo mf && mf.Name == "set_Target")
                     {
-                        yield return new(OpCodes.Call, typeof(ContextMenuOpenPatch).GetMethod(nameof(ContextMenuOpenPatch.SafeSetTarget)));
+                        yield return new(OpCodes.Call, typeof(ContextMenuOpenPatch).GetMethod(nameof(SafeSetTarget)));
+                    }
+                    else
+                    {
+                        yield return code;
+                    }
+                }
+            }
+        }
+
+        class RemoveLogMethodPatch
+        {
+            public static void FakeLog(string message, bool stackTrace) { }
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+            {
+                var patching = typeof(RemoveLogMethodPatch).GetMethod(nameof(FakeLog));
+                foreach (var code in codes)
+                {
+                    if (logMethods.Contains(code.operand))
+                    {
+                        yield return new(OpCodes.Call, patching);
                     }
                     else
                     {
